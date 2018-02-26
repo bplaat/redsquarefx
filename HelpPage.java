3caa227159b3052f5be9d3596710c2922791d1b6
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2018-02-26 12:22:05 +0100

    Add help page

--- HelpPage.java
+++ HelpPage.java
@@ -5,6 +5,12 @@ public class HelpPage extends Pane {
     HelpPage () {
         getChildren().addAll(
             Factory.getLabel("Help", 0, 20, "title", 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("RedSquareFX is an classic 2D action game.", 0, 90, 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("Click and hold the red square.", 0, 130, 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("Now move so that you neither touch the wall", 0, 170, 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("nor get hit by any of the blue squares.", 0, 210, 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("If you make it to 40 seconds, you are doing.", 0, 250, 640, Pos.BASELINE_CENTER),
+            Factory.getLabel("brilliantly!", 0, 290, 640, Pos.BASELINE_CENTER),
             Factory.getButton("Back", Page.HOME, (640 - 200) / 2, 410, 200, 50)
         );
     }
